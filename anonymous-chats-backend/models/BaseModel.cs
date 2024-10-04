48322da6232dfced9f10909f291b1f7c8f7c79d4
Rob Moore <moorer0523@gmail.com>
2024-10-03 20:01:39 -0400

    adding basemodel

---
+++ anonymous-chats-backend/models/BaseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace anonymous_chats_backend.Models;
+
+public abstract class BaseModel
+{
+    public string CreatedBy { get; set; } // User who created the entity
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow; // Timestamp for when the entity was created
+
+    public string UpdatedBy { get; set; } // User who last updated the entity
+    public DateTime? UpdatedOn { get; set; } // Nullable, because it may not have been updated yet
+}
